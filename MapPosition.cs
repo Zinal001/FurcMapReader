948319e1ea1d6f92d66ceeb99e7f28393eb0c0da
Zinal001 <LaNiZ.001@gmail.com>
2015-02-26 12:01:53 +0100

    Renamed Properties
    floornumber -> FloorNumber
    objectnumber -> ObjectNumber
    wallnumber -> WallNumber
    regionnumber -> RegionNumber
    effectnumber -> EffectNumber
    x -> X
    y -> Y
    
    All properties are now public gettable but only private settable

--- MapPosition.cs
+++ MapPosition.cs
@@ -7,24 +7,30 @@ namespace Zinal.FurcMapReader
 {
     public class MapPosition
     {
-        public ushort floornumber, objectnumber, wallnumber, regionnumber, effectnumber;
-        public int x, y;
+        public ushort FloorNumber { get; private set; }
+        public ushort ObjectNumber { get; private set; }
+        public ushort WallNumber { get; private set; }
+        public ushort RegionNumber { get; private set; }
+        public ushort EffectNumber { get; private set; }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
         public MapPosition(int x, int y)
         {
-            this.x = x;
-            this.y = y;
+            this.X = x;
+            this.Y = y;
         }
 
         public MapPosition(int x, int y, Map map)
         {
-            this.x = x;
-            this.y = y;
-            this.floornumber = map.getFloorAt(x, y);
-            this.objectnumber = map.getObjectAt(x, y);
-            this.wallnumber = map.getWallAt(x, y);
-            this.regionnumber = map.getRegionAt(x, y);
-            this.effectnumber = map.getEffectAt(x, y);
+            this.X = x;
+            this.Y = y;
+            this.FloorNumber = map.GetFloorAt(x, y);
+            this.ObjectNumber = map.GetObjectAt(x, y);
+            this.WallNumber = map.GetWallAt(x, y);
+            this.RegionNumber = map.GetRegionAt(x, y);
+            this.EffectNumber = map.GetEffectAt(x, y);
         }
     }
 }
--- MapTile.cs
+++ MapTile.cs
@@ -7,33 +7,40 @@ namespace Zinal.FurcMapReader
 {
     public class MapTile
     {
-        public ushort floorNumber, objectNumber, wallNENumber, wallNWNumber, regionNumber, effectNumber;
-        public int x, y;
+        public ushort FloorNumber { get; private set; }
+        public ushort ObjectNumber { get; private set; }
+        public ushort WallNENumber { get; private set; }
+        public ushort WallNWNumber { get; private set; }
+        public ushort RegionNumber { get; private set; }
+        public ushort EffectNumber { get; private set; }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
         public MapTile(int x, int y)
         {
-            this.x = x;
-            this.y = y;
+            this.X = x;
+            this.Y = y;
         }
 
         public MapTile(int x, int y, Map map)
         {
-            this.x = x;
-            this.y = y;
-            this.floorNumber = map.getFloorAt(x, y);
-            this.objectNumber = map.getObjectAt(x, y);
-            this.regionNumber = map.getRegionAt(x, y);
-            this.effectNumber = map.getEffectAt(x, y);
+            this.X = x;
+            this.Y = y;
+            this.FloorNumber = map.GetFloorAt(x, y);
+            this.ObjectNumber = map.GetObjectAt(x, y);
+            this.RegionNumber = map.GetRegionAt(x, y);
+            this.EffectNumber = map.GetEffectAt(x, y);
 
             if (x % 2 == 0)
             {
-                this.wallNENumber = map.getWallAt(x + 1, y);
-                this.wallNWNumber = map.getWallAt(x, y);
+                this.WallNENumber = map.GetWallAt(x + 1, y);
+                this.WallNWNumber = map.GetWallAt(x, y);
             }
             else
             {
-                this.wallNENumber = map.getWallAt(x, y);
-                this.wallNWNumber = map.getWallAt(x - 1, y);
+                this.WallNENumber = map.GetWallAt(x, y);
+                this.WallNWNumber = map.GetWallAt(x - 1, y);
             }
         }
     }
